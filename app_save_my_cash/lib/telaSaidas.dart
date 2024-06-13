9f13138575d0955fbe40d6e8caf70c4c81e45d1d
GizeleAlves <gizele0alves@gmail.com>
2024-06-12 23:41:40 -0300

    Concluindo o layout das telas de entradas e saídas.

--- app_save_my_cash/lib/telaSaidas.dart
+++ app_save_my_cash/lib/telaSaidas.dart
@@ -1,10 +1,12 @@
-import 'package:app_save_my_cash/telaLogin.dart';
+import 'telaLogin.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'telaConfiguracoes.dart';
 import 'telaEntradas.dart';
 import 'telaMetas.dart';
 import 'telaPerfil.dart';
 import 'telaResumo.dart';
+
 
 class TelaSaidas extends StatefulWidget {
   const TelaSaidas({super.key});
@@ -14,6 +16,38 @@ class TelaSaidas extends StatefulWidget {
 }
 
 class _TelaSaidasState extends State<TelaSaidas> {
+  DateTime _selectedDate = DateTime.now();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  String _formatDate(DateTime date) {
+    return DateFormat('dd/MM/yyyy').format(date);
+  }
+
+  void _incrementDate() {
+    setState(() {
+      _selectedDate = _selectedDate.add(Duration(days: 1));
+    });
+  }
+
+  void _decrementDate() {
+    setState(() {
+      _selectedDate = _selectedDate.subtract(Duration(days: 1));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,106 +59,128 @@ class _TelaSaidasState extends State<TelaSaidas> {
               ListTile(
                 leading: Icon(
                   Icons.bar_chart_outlined,
-                  color: Colors.blue, size: 40,
+                  color: Colors.blue,
+                  size: 40,
                 ),
-                title: Text('Resumo', style: TextStyle(fontSize: 22),),
+                title: Text(
+                  'Resumo',
+                  style: TextStyle(fontSize: 22),
+                ),
                 onTap: () {
                   print('Resumo');
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TelaResumo()),
-                    );
+                    context,
+                    MaterialPageRoute(builder: (context) => TelaResumo()),
+                  );
                 },
               ),
-
-               ListTile(
+              ListTile(
                 leading: Icon(
                   Icons.money_off_csred_outlined,
-                  color: Colors.red, size: 40,
+                  color: Colors.red,
+                  size: 40,
                 ),
-                title: Text('Saídas', style: TextStyle(fontSize: 22),),
+                title: Text(
+                  'Saídas',
+                  style: TextStyle(fontSize: 22),
+                ),
                 onTap: () {
                   print('Saídas');
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TelaSaidas()),
-                    );
+                    context,
+                    MaterialPageRoute(builder: (context) => TelaSaidas()),
+                  );
                 },
               ),
-
               ListTile(
                 leading: Icon(
                   Icons.monetization_on_outlined,
-                  color: Colors.green, size: 40,
+                  color: Colors.green,
+                  size: 40,
                 ),
-                title: Text('Entradas', style: TextStyle(fontSize: 22),),
+                title: Text(
+                  'Entradas',
+                  style: TextStyle(fontSize: 22),
+                ),
                 onTap: () {
                   print('Entradas');
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TelaEntradas()),
-                    );
+                    context,
+                    MaterialPageRoute(builder: (context) => TelaEntradas()),
+                  );
                 },
               ),
-
               ListTile(
                 leading: Icon(
                   Icons.auto_awesome_outlined,
-                  color: Colors.orange, size: 40,
+                  color: Colors.orange,
+                  size: 40,
                 ),
-                title: Text('Metas', style: TextStyle(fontSize: 22),),
+                title: Text(
+                  'Metas',
+                  style: TextStyle(fontSize: 22),
+                ),
                 onTap: () {
                   print('Metas');
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TelaMetas()),
-                    );
+                    context,
+                    MaterialPageRoute(builder: (context) => TelaMetas()),
+                  );
                 },
               ),
-
               ListTile(
                 leading: Icon(
                   Icons.settings,
-                  color: Colors.grey, size: 40,
+                  color: Colors.grey,
+                  size: 40,
                 ),
-                title: Text('Configurações', style: TextStyle(fontSize: 22),),
+                title: Text(
+                  'Configurações',
+                  style: TextStyle(fontSize: 22),
+                ),
                 onTap: () {
                   print('Configurações');
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TelaConfiguracoes()),
-                    );
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => TelaConfiguracoes()),
+                  );
                 },
               ),
-
               ListTile(
                 leading: Icon(
                   Icons.account_circle_outlined,
-                  color: Colors.purple, size: 40,
+                  color: Colors.purple,
+                  size: 40,
                 ),
-                title: Text('Perfil', style: TextStyle(fontSize: 22),),
+                title: Text(
+                  'Perfil',
+                  style: TextStyle(fontSize: 22),
+                ),
                 onTap: () {
                   print('Perfil');
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TelaPerfil()),
-                    );
-                  
+                    context,
+                    MaterialPageRoute(builder: (context) => TelaPerfil()),
+                  );
                 },
               ),
-
               ListTile(
                 leading: Icon(
                   Icons.output_outlined,
-                  color: Colors.black, size: 40,
+                  color: Colors.black,
+                  size: 40,
                 ),
-                title: Text('Sair', style: TextStyle(fontSize: 22),),
+                title: Text(
+                  'Sair',
+                  style: TextStyle(fontSize: 22),
+                ),
                 onTap: () {
                   print('Sair');
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TelaLogin()),
-                    );
+                    context,
+                    MaterialPageRoute(builder: (context) => TelaLogin()),
+                  );
                 },
               ),
             ],
@@ -133,24 +189,109 @@ class _TelaSaidasState extends State<TelaSaidas> {
         appBar: AppBar(
           title: Center(
             child: Text(
-              'Página Inicial',
+              'Saídas',
               style:
                   TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
             ),
           ),
           backgroundColor: Color.fromRGBO(48, 203, 128, 50),
           iconTheme: IconThemeData(color: Colors.white),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {
+                _selectDate(context);
+              },
+              icon: Icon(Icons.calendar_month_outlined),
+            )
+          ],
         ),
         backgroundColor: Color.fromARGB(255, 221, 255, 222),
         body: Padding(
-          padding: EdgeInsets.all(25.0),
+          padding: EdgeInsets.all(16.0),
           child: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text("Saídas"),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Dia',
+                          style: TextStyle(fontSize: 22, color: Colors.black),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Mês',
+                          style: TextStyle(fontSize: 22, color: Colors.black),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Ano',
+                          style: TextStyle(fontSize: 22, color: Colors.black),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Período',
+                          style: TextStyle(fontSize: 22, color: Colors.black),
+                        ),
+                      ),
+                    ]),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: _decrementDate,
+                      icon: Icon(
+                        Icons.arrow_left_outlined,
+                        size: 50,
+                      ),
+                    ),
+                    Text(
+                      '${_formatDate(_selectedDate!)}',
+                      style: TextStyle(fontSize: 22),
+                    ),
+                    IconButton(
+                      onPressed: _incrementDate,
+                      icon: Icon(
+                        Icons.arrow_right_outlined,
+                        size: 50,
+                      ),
+                    ),
+                  ],
+                ),
                 
-                
+
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 10, // Número de cartões que você deseja exibir
+                    itemBuilder: (context, index) {
+                      return Card(
+                        margin: EdgeInsets.symmetric(vertical: 8.0),
+                        child: ListTile(
+                          title: Text('Título da Sáida'),
+                          subtitle: Text('Categoria\nR\$ 0,00'),
+                          trailing: Icon(Icons.more_vert_outlined),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                IconButton(
+                      onPressed:(){},
+                      icon: Icon(
+                        Icons.add_box_outlined,
+                        size: 80, color: Color.fromRGBO(48, 203, 128, 50),
+                      ),
+                    ),
+
               ],
             ),
           ),
